eddd5dca990b94e02b5784bae57bb3c1b1178552
Phanindra Reddigari <reddph@gmail.com>
2016-01-24 17:19:38 -0600

    Adding more help explanation for progress bar
    Adding more help explanation for progress bar

--- ui.R
+++ ui.R
@@ -7,7 +7,8 @@ shinyUI(
   fluidPage(
       titlePanel("NLP Word Prediction"),
       
-      div("This is a simple UI for accepting a phrase from the user and returning the phrase with the next predicted word in the phrase. Please enter your phrase in the box below, and when you are ready, press the Submit button for prediction. You can edit text or erase text in the box any time to enter a new phrase for prediction. The UI selects an example phrase for prediction during the initial launch for demonstrating the basic input/output mechanism.",
+      div("This is a simple UI for accepting a phrase from the user and returning the phrase with the next predicted word in the phrase. Please enter your phrase in the box below, and when you are ready, press the Submit button for prediction. You can edit the text or erase text in the box any time to enter a new phrase for prediction. The UI selects an example phrase for prediction during the initial launch for demonstrating the basic input/output mechanism. The UI takes 15 to 20 seconds to load the training data sets for n-Grams. Please be patient to allow the initialization to complete. The UI is ready to use once the progress bar starts and closes, and the output box (blue color background) shows the completed sample phrase.
+",
           style="font-sil6pt; font-weight:bold"),
   
       # helpText(h4("This is a simple UI for accepting a phrase from the user and returning the phrase with the next predicted word in the sentence. Please enter your phrase in the box below, and when you are ready, press the Submit button for prediction. You can edit text or erase text in the box any time to enter a new phrase for prediction. The UI selects an example phrase for prediction during the initial launch for demonstrating the basic input/output mechanism")),
